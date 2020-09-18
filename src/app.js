4c6bf70f09a6f37b990372142aadbbb6c6971fb3
Graham Wallace <grahamjwallace@gmail.com>
2020-09-18 15:01:15 +0100

    After explore JSX

--- src/app.js
+++ src/app.js
@@ -1,4 +1,22 @@
-let template = <h1>Indecision app</h1>;  
+let template = (
+    <div>
+        <h1>Indecision app</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);  
+
+let templateTwo = (
+    <div>
+        <h1>GJW</h1>
+        <p>Age: 100</p>
+        <p>Location: My location</p>
+    </div>
+);
+
 let appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(templateTwo, appRoot)
