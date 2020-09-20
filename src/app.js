40b955b207933bfda7761051d2c6a2b629c5c862
Graham Wallace <grahamjwallace@gmail.com>
2020-09-20 08:14:59 +0100

    After conditional rendering in JSX

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 let app = {
     title: 'Indecision app',
-    subtitle : 'Use the computer'
+    subtitle: 'Use the computer',
+    options: ['One','Two'] 
 }
 
 let template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options && app.options.length > 0 ? 'Options' : 'No options'}</p>
         <ol>
             <li>Item one</li>
             <li>Item two</li>
@@ -14,11 +16,24 @@ let template = (
     </div>
 );  
 
+var user = {
+    name: 'XYZ',
+    age: 18,
+    location: 'here'
+}
+
+function getLocation (location) {
+    if(location) {
+        return <p>Location: {location}</p>
+    }
+}
+
 let templateTwo = (
     <div>
-        <h1>GJW</h1>
-        <p>Age: 100</p>
-        <p>Location: My location</p>
+        <h1>{user.name ? user.name: 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+        {<h3>My H3</h3>}
     </div>
 );
 
