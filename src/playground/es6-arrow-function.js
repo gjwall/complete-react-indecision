a747a4b253f68d8c5a3b0523bff573480242d132
Graham Wallace <grahamjwallace@gmail.com>
2020-09-20 13:38:41 +0100

    After first arrow functions video

---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,9 @@
+const getFirstNameF = (fullName) => {
+    return fullName.split(' ')[0];
+}
+
+const getFirstName = (fullName) => fullName.split(' ')[0];
+
+console.log(getFirstNameF('Bob bobby'));
+
+console.log(getFirstName('Abe Abey'));
