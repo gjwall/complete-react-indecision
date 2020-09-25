89b6a5152c97067bf3c07efa6e945206cadf6378
Graham Wallace <grahamjwallace@gmail.com>
2020-09-25 13:48:06 +0100

    After installing and configuring webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// entry point and output bundle
+// https://webpack.js.org/concepts/#entry
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    mode: 'development'
+};
