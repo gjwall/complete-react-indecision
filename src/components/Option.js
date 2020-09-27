6f96351c8b1d03af96ae31afd2a063107c716168
Graham Wallace <grahamjwallace@gmail.com>
2020-09-27 17:04:45 +0100

    After styling option item

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Option = (props) => (
-    <div>
-        {props.optionText}
+    <div className="option">
+        <p className="option__text">{props.count}.{props.optionText}</p>
         <button 
-        // This way does not pass in the proper arguments
-        //onClick={props.handleDeleteOption}
-        // This way does pass in the arguments
+            // This way does not pass in the proper arguments
+            //onClick={props.handleDeleteOption}
+            // This way does pass in the arguments
             className="button button--link"
             onClick={(e) => {
                 props.handleDeleteOption(props.optionText);
